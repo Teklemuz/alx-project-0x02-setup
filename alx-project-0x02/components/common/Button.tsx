5f7a807148367f4fbfa071869c13be1952f0fec6
Teklemuz Ayenw <111185891+Teklemuz@users.noreply.github.com>
2025-01-09 17:20:48 +0300

    Create Button.tsx

---
+++ alx-project-0x02/components/common/Button.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ButtonProps } from '../../interfaces';
+
+const Button: React.FC<ButtonProps> = ({ size, shape, children, onClick }) => {
+  const baseStyles = 'px-4 py-2 font-semibold text-white';
+
+  const sizeStyles = {
+    small: 'text-sm',
+    medium: 'text-base',
+    large: 'text-lg',
+  };
+
+  const shapeStyles = {
+    'rounded-sm': 'rounded-sm',
+    'rounded-md': 'rounded-md',
+    'rounded-full': 'rounded-full',
+  };
+
+  const buttonStyles = `${baseStyles} ${sizeStyles[size]} ${shapeStyles[shape]} bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50`;
+
+  return (
+    <button className={buttonStyles} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
