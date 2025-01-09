c615e0f2b24edf49a80ffe620d6dfafe59cc5b03
Teklemuz Ayenw <111185891+Teklemuz@users.noreply.github.com>
2025-01-09 17:31:29 +0300

    Create index.tsx

---
+++ alx-project-0x02/pages/index.tsx
@@ -0,0 +1,14 @@
+import Header from '../components/layout/Header';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <main className="p-8">
+        <h2 className="text-2xl">Welcome to the ALX Project 2!</h2>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
