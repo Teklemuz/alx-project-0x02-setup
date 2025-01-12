8ce1eb845e6b9f6aa6009edacc5bebaf0a1fee75
Teklemuz Ayenw <111185891+Teklemuz@users.noreply.github.com>
2025-01-12 15:20:49 +0300

    Update users.tsx

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,25 +1,51 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Header from '../components/layout/Header';
 import UserCard from '../components/common/UserCard';
 
-const UsersPage: React.FC = () => {
-  const [users, setUsers] = useState([]);
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then(setUsers);
-  }, []);
+interface UsersPageProps {
+  users: User[];
+}
 
+const UsersPage: React.FC<UsersPageProps> = ({ users }) => {
   return (
     <div>
       <Header />
       <h1 className="text-3xl mt-6 mb-4">Users</h1>
-      {users.map((user: any) => (
+      {users.map((user) => (
         <UserCard key={user.id} {...user} />
       ))}
     </div>
   );
 };
+
+// This function will run at build time and fetch the users
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users = await res.json();
+
+  return {
+    props: { users },
+  };
+}
 
 export default UsersPage;
